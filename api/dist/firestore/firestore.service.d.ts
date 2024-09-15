1400a2d3816a9969c5d8d7e55719bcb64d016682
Sebastian <sebastianbeltran341@gmail.com>
2024-09-15 11:37:37 -0500

    vercel

--- api/dist/firestore/firestore.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as admin from 'firebase-admin';
-export declare class FirestoreService {
-    constructor();
-    getFirestore(): admin.firestore.Firestore;
-    getStorage(): import("@google-cloud/storage").Bucket;
-}
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`App running on port ${port}`);
 }
